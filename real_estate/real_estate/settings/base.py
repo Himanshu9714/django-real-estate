3a9a87acbb538b851b09cda960096d662e72637b
Himanshu Suthar <h9714348433@gmail.com>
2024-06-12 18:20:24 +0530

    User authentication

--- real_estate/real_estate/settings/base.py
+++ real_estate/real_estate/settings/base.py
@@ -4,6 +4,7 @@ import logging.config
 
 from django.utils.log import DEFAULT_LOGGING
 from pathlib import Path
+from datetime import timedelta as td
 
 env = environ.Env(DEBUG=(bool, False))
 
@@ -43,6 +44,8 @@ THIRD_PARTY_APPS = [
     "django_filters",
     "django_countries",
     "phonenumber_field",
+    "djoser",
+    "rest_framework_simplejwt",
 ]
 
 INHOUSE_APPS = [
@@ -145,6 +148,45 @@ MEDIA_ROOT = BASE_DIR / "mediafiles"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "users.User"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    )
+}
+
+SIMPLE_JWT = {
+    "AUTH_HEADER_TYPES": (
+        "Bearer",
+        "JWT",
+    ),
+    "ACCESS_TOKEN_LIFETIME": td(minutes=120),
+    "REFRESH_TOKEN_LIFETIME": td(days=1),
+    "SIGNING_KEY": env("SIGNING_KEY"),
+    "AUTH_HEADER_NAME": "HTTP_AUTHORIZATION",
+    "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
+}
+
+DJOSER = {
+    "LOGIN_FIELD": "email",
+    "USER_CREATE_PASSWORD_RETYPE": True,
+    "USERNAME_CHANGED_EMAIL_CONFIRMATION": True,
+    "PASSWORD_CHANGED_EMAIL_CONFIRMATION": True,
+    "SEND_CONFIRMATION_EMAIL": True,
+    "PASSWORD_RESET_CONFIRM_URL": "password/reset/confirm/{uid}/{token}",
+    "SET_PASSWORD_RETYPE": True,
+    "PASSWORD_RESET_CONFIRM_RETYPE": True,
+    "USERNAME_RESET_CONFIRM_URL": "email/reset/confirm/{uid}/{token}",
+    "ACTIVATION_URL": "activate/{uid}/{token}",
+    "SEND_ACTIVATION_EMAIL": True,
+    "SERIALIZERS": {
+        "user_create": "apps.users.serializers.CreateUserSerializer",
+        "user": "apps.users.serializers.UserSerializer",
+        "current_user": "apps.users.serializers.CurrentUserSerializer",
+        "current_user": "apps.users.serializers.CurrentUserSerializer",
+        "user_delete": "apps.users.serializers.UserDeleteSerializer",
+    },
+}
 
 # Logging
 logger = logging.getLogger(__name__)
