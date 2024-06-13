f9b67a403f77c007412ff8d52043530d5309b76e
Himanshu Suthar <h9714348433@gmail.com>
2024-06-13 11:23:22 +0530

    APIs for enquiry and ratings

--- real_estate/apps/enquiries/views.py
+++ real_estate/apps/enquiries/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.core.mail import send_mail
+from django.conf import settings
+from rest_framework import permissions, status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Enquiry
+
+default_email = settings.DEFAULT_FROM_EMAIL
+
+
+@api_view(["POST"])
+@permission_classes([permissions.AllowAny])
+def send_enquiry_email(request):
+    data = request.data
+
+    try:
+        subject = data["subject"]
+        name = data["name"]
+        email = data["email"]
+        message = data["message"]
+        from_email = data["email"]
+        recipients = [default_email]
+
+        send_mail(subject, message, from_email, recipients, fail_silently=True)
+        enquiry = Enquiry(name=name, email=email, subject=subject, message=message)
+        enquiry.save()
+
+        return Response({"success": "Your enquiry was sent successfully!"})
+
+    except:
+        return Response(
+            {"fail": "Enquiry was not sent. Please try again"},
+            status=status.HTTP_200_OK,
+        )
